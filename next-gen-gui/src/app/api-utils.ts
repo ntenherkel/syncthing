c4673f0f065a0ce8e7ad85d960781b5b10721926
Niels ten Herkel <niels.ten.herkel@rabobank.nl>
2023-01-20 17:27:02 +0100

    Reformat folders

--- next-gen-gui/src/app/api-utils.ts
+++
@@ -1,9 +0,0 @@
-import { environment } from '../environments/environment'
-
-export const deviceID = (): String => {
-    const dID: String = environment.production ? globalThis.metadata['deviceID'] : '12345';
-    return dID.substring(0, 5)
-}
-
-export const apiURL: String = '/'
-export const apiRetry: number = 3;
